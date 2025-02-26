e46e7398be0d63d6b8d2ba75017d1e7ac9543828
jnakht <jisan.cse.cu@gmail.com>
2025-02-26 16:10:05 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+ 
+
+  return (
+    <>
+        <h1 className='text-4xl text-red-500 font-bold'>hello world </h1>
+        <h3>the world is fine but not world</h3>
+        <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
